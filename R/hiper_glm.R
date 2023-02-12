0a8754a85293e1ebd196aabfa509861f7370f4d4
Yuzheng Dun <yuzhengdun@Yuzhengs-MacBook-Pro.local>
2023-02-12 13:08:24 -0500

    finish hw1

---
+++ R/hiper_glm.R
@@ -0,0 +1,8 @@
+#' @export
+hiper_glm <- function(design, outcome){
+  warning("`hiper_glm` is yet to be implemented.")
+  #TODO: implementation
+  hglm_out <- list()
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
+}
